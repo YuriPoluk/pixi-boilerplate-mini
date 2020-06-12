eef44808410f01dc3d84d0a4e1a503b33afff796
Yuri Poluk <polukyu@gmail.com>
2020-06-12 17:40:57 +0300

    improved gitignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles/styles.css'
-import LayoutManager from './libs/LayoutManager.js';
 import GameController from './GameController.js';
 
 GameController.init(document.getElementById("scene"));
